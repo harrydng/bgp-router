87ceeee439f40aa262a123e13802cca53d7e19d4
Osvetamp4 <82180979+Osvetamp4@users.noreply.github.com>
2026-02-18 17:53:09 -0500

    worked on update function

--- RIB.py
+++ RIB.py
@@ -1,8 +1,18 @@
 def handshake(socket, jsonObject):
     pass
 
-def update(socket, jsonObject):
-    pass
+def netmask_to_prefixlen(netmask):
+    quads = [int(qdn) for qdn in netmask.split('.')]
+    mask_int = (quads[0] << 24) + (quads[1] << 16) + (quads[2] << 8) + quads[3]
+    return bin(mask_int).count('1')
+
+def update(socket, jsonObject, routeTable):
+    network = jsonObject["msg"]["network"]
+    netmask = jsonObject["msg"]["netmask"]
+    ASpath = jsonObject["msg"]["ASpath"]
+    prefixlen = netmask_to_prefixlen(netmask)
+    routeTable[network + "/" + str(prefixlen)] = jsonObject["msg"]
+    return routeTable
 
 def withdraw(socket, jsonObject):
     pass
